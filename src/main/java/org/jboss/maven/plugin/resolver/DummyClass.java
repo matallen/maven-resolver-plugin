4f52c08bd79b1622b8dbaeab0ba621ad79b89cb1
keithlynch <keith@redhat.com>
2014-05-15 08:34:32 +0100

    Update to twdata version 2.2.0 and maven 3

--- src/main/java/org/jboss/maven/plugin/resolver/DummyClass.java
+++ src/main/java/org/jboss/maven/plugin/resolver/DummyClass.java
@@ -1,16 +1,7 @@
 package org.jboss.maven.plugin.resolver;
 
-import java.io.File;
-
 /**
- * 
- * This is a purposely sparse class, intended to be used as a place holder for
- * runtime provided, composite dependencies
- * 
- * @author graham@redhat.com
- * 
+ * Created by keithlynch on 15/05/2014.
  */
 public class DummyClass {
-
-  
 }
